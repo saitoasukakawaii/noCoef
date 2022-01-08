0067e8f8b98e2861d489e41af95e5c608ec059ab
saitoasukakawaii <389641222@qq.com>
2022-01-08 06:38:34 -0800

    1

--- arteries.h
+++ arteries.h
@@ -49,6 +49,7 @@ public:
 
   double *Qnew, *Qold, *Qh,    // The arrays needed to store data during
          *Anew, *Aold, *Ah,    // the numerical solution of the system.
+         *SLnew, *SLold,
          *R1, *R2, *R1h, *R2h,
          *S1, *S2, *S1h, *S2h,
          *Qprv, *Aprv,
